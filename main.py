89414d09c6b87991b399be710afc2abdc811ca07
Mvhamad <bamohamed961@gmail.com>
2024-02-07 12:15:17 +0000

    atelier streamlit

---
+++ main.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("Bienvenue dans mon atelier streamlit")
+st.text("C'est notre premier atelier streamlit.\n"
+        "J'espère que vous allez passez d'excellent moment avec nous")
