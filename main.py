193c232925fb1aea445371d42895f2bca64103fe
Mvhamad <bamohamed961@gmail.com>
2024-02-08 12:02:02 +0000

    add texte

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import joblib
 st.title("Bienvenue dans mon atelier streamlit")
 st.text("C'est notre premier atelier streamlit.\n"
         "J'espère que vous allez passez d'excellent moment avec nous")
+st.text("Merci de votre présence")
